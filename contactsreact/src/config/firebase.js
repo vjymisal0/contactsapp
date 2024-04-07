d768bf52d3a6118b26bcfb4c4d690795b12b2969
Vijay Misal <misalvijay153@gmail.com>
2024-04-07 10:56:36 +0530

    firebase setup

---
+++ contactsreact/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCFfJr2nALL5a9y2Q-_P-bhW-g1Qo2Q1ZM",
+    authDomain: "contactsapp-eb167.firebaseapp.com",
+    projectId: "contactsapp-eb167",
+    storageBucket: "contactsapp-eb167.appspot.com",
+    messagingSenderId: "173723005657",
+    appId: "1:173723005657:web:6ebb86dcce45af58a3c4c8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
