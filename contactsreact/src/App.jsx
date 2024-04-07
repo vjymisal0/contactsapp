c753032a27ce9ee3dd6544de0cafbf733279eb33
Vijay Misal <misalvijay153@gmail.com>
2024-04-07 22:03:09 +0530

    88888888888

--- contactsreact/src/App.jsx
+++ contactsreact/src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+
 const App = () => {
   return (
-    <div className="mx-auto max-w-[370px]">
+    <div className="mx-auto max-w-[370px] px-4">
       <Navbar />
+      <div className="flex">
+        <FiSearch className="text-white" />
+        <input
+          type="text"
+          className="h-10 flex-grow bg-transparent  border-white rounded-md border"
+        />
+      </div>
     </div>
   );
 };
--- contactsreact/src/components/Navbar.jsx
+++ contactsreact/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="m-4 h-[60px] rounded-lg bg-white flex items-center justify-center gap-2 text-xl font-medium ">
+    <div className="my-4 h-[60px] rounded-lg bg-white flex items-center justify-center gap-2 text-xl font-medium">
       <img src="../public/firebase_logo.svg" alt="" />
       <h1>Firebase Contact App</h1>
     </div>
