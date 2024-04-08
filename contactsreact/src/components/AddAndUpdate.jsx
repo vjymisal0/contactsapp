bf666f7aded546cc7102d83b0e317c2a2e3f6d0a
Vijay Misal <misalvijay153@gmail.com>
2024-04-09 00:04:22 +0530

    form update

--- contactsreact/src/components/AddAndUpdate.jsx
+++ contactsreact/src/components/AddAndUpdate.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import { Form, Formik, Field } from "formik";
 import Modal from "./Modal";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 const AddAndUpdate = ({ isOpen, onClose }) => {
+  const addContact = (contact) => {
+    try {
+      const contactRef = collection(db, "contacts");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <Modal isOpen={isOpen} onClose={onClose}>
-        <Formik>
+        <Formik
+          initialValues={{ name: "", email: "" }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+        >
           <Form className="flex flex-col gap-4">
             <div className="flex flex-col gap-1">
               <label htmlFor="name">Name</label>
-              <Field name="name" className="border h-10 rounded-lg" />
+              <Field
+                name="name"
+                className="border h-10 text-black rounded-lg"
+              />
             </div>
             <div className="flex flex-col gap-1">
               <label htmlFor="email">Email</label>
               <Field
-                type="email"
                 name="email"
-                className="border h-10 rounded-lg"
+                className="border h-10  text-black rounded-lg"
               />
             </div>
             <button className="bg-blue px-3 py-1 border rounded-lg self-end">
