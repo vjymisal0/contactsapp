6cd76d73ae40e0797cb1f296ce87b64c2786e9ea
Vijay Misal <misalvijay153@gmail.com>
2024-04-08 13:12:46 +0530

    firebase

--- contactsreact/src/App.jsx
+++ contactsreact/src/App.jsx
@@ -4,6 +4,9 @@ import { FiSearch } from "react-icons/fi";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./config/firebase";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { RiEditCircleLine } from "react-icons/ri";
+import { IoMdTrash } from "react-icons/io";
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
@@ -20,6 +23,7 @@ const App = () => {
           };
         });
         setContacts(contactLists); // Set contacts state with fetched data
+        console.log("Contacts fetched successfully!", contactLists);
       } catch (error) {
         console.error("Error fetching contacts:", error);
       }
@@ -40,14 +44,26 @@ const App = () => {
         </div>
         <AiFillPlusCircle className="text-white text-5xl cursor-pointer" />
       </div>
-      {/* Display contacts */}
-      <ul>
+      <div>
         {contacts.map((contact) => (
-          <li key={contact.id}>
-            {contact.name} - {contact.email}
-          </li>
+          <div key={contact.id} className="bg-gray rounded-xl p-2">
+            <div className="flex">
+              <HiOutlineUserCircle className="text-white text-4xl" />
+              <div className="text-white">
+                <div className="">
+                  <h2 className="">{contact.name}</h2>
+                  <p className="">{contact.email}</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <RiEditCircleLine />
+
+              <IoMdTrash />
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
--- contactsreact/tailwind.config.js
+++ contactsreact/tailwind.config.js
@@ -8,10 +8,9 @@ export default {
     extend: {
       colors: {
         gray: "#5A5959",
-        blue: "3F8BFC",
-        yellow: "#AEFFFF",
-        orange: "#000000",
-        purple: "5A5959",
+        blue: "#3F8BFC",
+        light_cyan: "#AEFFFF",
+        dark_black: "#000000",
         red: "#D01C28"
       }
     },
